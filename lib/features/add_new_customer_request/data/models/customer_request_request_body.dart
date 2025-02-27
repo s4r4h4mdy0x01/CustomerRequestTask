2a995cd0561a4e340af5e176555502d2c5935874
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2025-02-27 14:06:05 +0200

    Add CustomerRequestRequestBody Model

---
+++ lib/features/add_new_customer_request/data/models/customer_request_request_body.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'customer_request_request_body.g.dart';
+@JsonSerializable()
+class CustomerRequestRequestBody {
+  final String name;
+  final String phoneNumber;
+  final String taxNumber;
+  final double creditLimit;
+  final String buildingNumber;
+  final String streetName;
+  final int subNumber;
+  final String zipCode;
+  final String cityEnName;
+  final String cityArName;
+  final String countryEnName;
+  final String countryArName;
+  final List<String> files;
+
+  CustomerRequestRequestBody({required this.name, required this.phoneNumber, required this.taxNumber, required this.creditLimit, required this.buildingNumber, required this.streetName, required this.subNumber, required this.zipCode, required this.cityEnName, required this.cityArName, required this.countryEnName, required this.countryArName, required this.files});
+   Map<String, dynamic> toJson() => _$CustomerRequestRequestBodyToJson(this);
+}
---
+++ lib/features/add_new_customer_request/data/models/customer_request_request_body.g.dart
@@ -0,0 +1,43 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'customer_request_request_body.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CustomerRequestRequestBody _$CustomerRequestRequestBodyFromJson(
+  Map<String, dynamic> json,
+) => CustomerRequestRequestBody(
+  name: json['name'] as String,
+  phoneNumber: json['phoneNumber'] as String,
+  taxNumber: json['taxNumber'] as String,
+  creditLimit: (json['creditLimit'] as num).toDouble(),
+  buildingNumber: json['buildingNumber'] as String,
+  streetName: json['streetName'] as String,
+  subNumber: (json['subNumber'] as num).toInt(),
+  zipCode: json['zipCode'] as String,
+  cityEnName: json['cityEnName'] as String,
+  cityArName: json['cityArName'] as String,
+  countryEnName: json['countryEnName'] as String,
+  countryArName: json['countryArName'] as String,
+  files: (json['files'] as List<dynamic>).map((e) => e as String).toList(),
+);
+
+Map<String, dynamic> _$CustomerRequestRequestBodyToJson(
+  CustomerRequestRequestBody instance,
+) => <String, dynamic>{
+  'name': instance.name,
+  'phoneNumber': instance.phoneNumber,
+  'taxNumber': instance.taxNumber,
+  'creditLimit': instance.creditLimit,
+  'buildingNumber': instance.buildingNumber,
+  'streetName': instance.streetName,
+  'subNumber': instance.subNumber,
+  'zipCode': instance.zipCode,
+  'cityEnName': instance.cityEnName,
+  'cityArName': instance.cityArName,
+  'countryEnName': instance.countryEnName,
+  'countryArName': instance.countryArName,
+  'files': instance.files,
+};
