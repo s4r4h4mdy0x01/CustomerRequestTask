ecc2e2291f1103f59d611d683795dffdc2256cba
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2025-02-27 12:24:10 +0200

    Add system design to the structure

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class AddNewCustomerRequest extends StatelessWidget {
+ 
+  const AddNewCustomerRequest({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
