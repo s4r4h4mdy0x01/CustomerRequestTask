ce52bdcaa9391fc1e4f94ea799b9c8c5ce5e2eb7
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2025-03-17 10:04:17 +0200

    Refactor Employee screen

--- lib/core/widgets/custom_elevated_button_app.dart.dart
+++ lib/core/widgets/custom_elevated_button_app.dart.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -6,22 +5,26 @@ class CustomElevatedButtonApp extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
   final Color color;
+  final Size? size;
 
-  const CustomElevatedButtonApp({super.key, required this.text, required this.onPressed , required this.color});
+  const CustomElevatedButtonApp({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    required this.color,
+    this.size,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
+        minimumSize: size,
         backgroundColor: color,
-        padding:  EdgeInsets.symmetric(horizontal: 50.w, vertical: 10.h),
+        padding: EdgeInsets.symmetric(horizontal: 50.w, vertical: 10.h),
       ),
-      child: Text(
-        text,
-        style:  TextStyle(fontSize: 22.sp, color: Colors.white),
-      ),
+      child: Text(text, style: TextStyle(fontSize: 22.sp, color: Colors.white)),
     );
   }
 }
-
--- lib/features/employee/employee_screen.dart
+++ lib/features/employee/employee_screen.dart
@@ -1,10 +1,51 @@
+import 'package:customer_request_task/core/helper/spacing.dart';
+import 'package:customer_request_task/core/theme/color_manager.dart';
+import 'package:customer_request_task/core/widgets/custom_elevated_button_app.dart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EmployeeScreen extends StatelessWidget {
   const EmployeeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('الموظف : حسن '),
+        backgroundColor: ColorManager.primary,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+          child: Column(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              verticalSpace(60),
+              CustomElevatedButtonApp(
+                color: ColorManager.primary,
+                onPressed: () {},
+                text: 'حضور وإنصراف',
+                size: Size(double.infinity, 50),
+              ),
+              verticalSpace(20),
+              CustomElevatedButtonApp(
+                color: ColorManager.primary,
+                onPressed: () {},
+                text: 'طلب إجازة ',
+                size: Size(double.infinity, 50),
+              ),
+              verticalSpace(20),
+              CustomElevatedButtonApp(
+                color: ColorManager.primary,
+                onPressed: () {},
+                text: 'طلب سلفة ',
+                size: Size(double.infinity, 50),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
